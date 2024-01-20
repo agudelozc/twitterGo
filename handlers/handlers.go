56a1f6c339d3b7f6d09e70e65970c506608adf9b
CRISTIAN <agudelozc@gmail.com>
2024-01-20 12:51:06 -0500

    Ultimo commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -34,9 +34,11 @@ func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) mod
 		switch ctx.Value(models.Key("path")).(string) {
 		case "registro":
 			return routers.Registro(ctx)
-		}
 		case "login":
 			return routers.Login(ctx)
+		case "tweet":
+			return routers.GraboTweet(ctx, claim)
+		}
 		
 	case "PUT":
 		switch ctx.Value(models.Key("path")).(string) {
---
+++ routers/graboTweet.go
@@ -0,0 +1,42 @@
+package routers
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+	"github.com/agudelozc/twitterGo/bd"
+	"github.com/agudelozc/twitterGo/models"
+)
+
+func GraboTweet(ctx context.Context, claim models.Claim) models.RespApi{
+	var mensaje models.Tweet
+
+	var r models.RespApi
+	r.Status = 400
+	IDUsuario := claim.ID.Hex()
+
+	body := ctx.Value(models.Key("body")).(string)
+	err := json.Unmarshal([]byte(body), &mensaje)
+	if err != nil{
+		r.Message="Ocurrio un error al intentar decodificar el body"+err.Error()
+		return r
+	}
+	registro := models.GraboTweet{
+		UserID: IDUsuario,
+		Mensaje: mensaje.Mensaje,
+		Fecha: time.Now(),
+	}
+
+	_, status, err := bd.InsertoTweet(registro)
+	if err != nil{
+		r.Message="Ocurrio un error al insertar el registro"+err.Error()
+		return r
+	}
+	if !status{
+		r.Message="No se ha logrado insertar el tweet"
+		return r
+	}
+	r.Status=200
+	r.Message="Tweet creado correctamente"
+	return r
+}
